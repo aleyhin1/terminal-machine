5f4ec61997531aa1455d9dfeeb403a7d412524fe
Batuhan Akyürek <batuhan@loopgames.net>
2026-03-15 11:56:35 +0300

    more registers with more bits added

--- memory.c
+++ memory.c
@@ -8,7 +8,43 @@ Register1* register1_create()
     return r;
 }
 
+Register2* register2_create()
+{
+    Register2* r;
+    r = calloc(1, sizeof(Register2));
+    return r;
+}
+
+Register4* register4_create()
+{
+    Register4* r;
+    r = calloc(1, sizeof(Register4));
+    return r;
+}
+
+Register8* register8_create()
+{
+    Register8* r;
+    r = calloc(1, sizeof(Register8));
+    return r;
+}
+
 void register1_destroy(Register1* r)
+{
+    free(r);
+}
+
+void register2_destroy(Register2* r)
+{
+    free(r);
+}
+
+void register4_destroy(Register4* r)
+{
+    free(r);
+}
+
+void register8_destroy(Register8* r)
 {
     free(r);
 }
@@ -20,7 +56,54 @@ void register1_write(Register1* r, bool i, bool ck)
     *r = reg;
 }
 
+void register2_write(Register2* r, Bit2 i, bool ck)
+{
+    Register2 reg = *r;
+    reg.data.bit0 = (i.bit0 && ck) || (reg.data.bit0 && !ck);
+    reg.data.bit1 = (i.bit1 && ck) || (reg.data.bit1 && !ck);
+    *r = reg;
+}
+
+void register4_write(Register4* r, Bit4 i, bool ck)
+{
+    Register4 reg = *r;
+    reg.data.bit0 = (i.bit0 && ck) || (reg.data.bit0 && !ck);
+    reg.data.bit1 = (i.bit1 && ck) || (reg.data.bit1 && !ck);
+    reg.data.bit2 = (i.bit2 && ck) || (reg.data.bit2 && !ck);
+    reg.data.bit3 = (i.bit3 && ck) || (reg.data.bit3 && !ck);
+    *r = reg;
+}
+
+void register8_write(Register8* r, Bit8 i, bool ck)
+{
+    Register8 reg = *r;
+    reg.data.bit0 = (i.bit0 && ck) || (reg.data.bit0 && !ck);
+    reg.data.bit1 = (i.bit1 && ck) || (reg.data.bit1 && !ck);
+    reg.data.bit2 = (i.bit2 && ck) || (reg.data.bit2 && !ck);
+    reg.data.bit3 = (i.bit3 && ck) || (reg.data.bit3 && !ck);
+    reg.data.bit4 = (i.bit4 && ck) || (reg.data.bit4 && !ck);
+    reg.data.bit5 = (i.bit5 && ck) || (reg.data.bit5 && !ck);
+    reg.data.bit6 = (i.bit6 && ck) || (reg.data.bit6 && !ck);
+    reg.data.bit7 = (i.bit7 && ck) || (reg.data.bit7 && !ck);
+    *r = reg;
+}
+
 bool register1_read(Register1* r)
 {
     return (*r).bit0;
+}
+
+Bit2 register2_read(Register2* r)
+{
+    return (*r).data;
+}
+
+Bit4 register4_read(Register4* r)
+{
+    return (*r).data;
+}
+
+Bit8 register8_read(Register8* r)
+{
+    return (*r).data;
 }
--- memory.h
+++ memory.h
@@ -1,19 +1,44 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+#include "bits.h"
 #include <stdbool.h>
 
 typedef struct 
 {
     bool bit0;
 } Register1;
+typedef struct 
+{
+    Bit2 data;
+} Register2;
+typedef struct 
+{
+    Bit4 data;
+} Register4;
+typedef struct 
+{
+    Bit8 data;
+} Register8;
 
 Register1* register1_create();
+Register2* register2_create();
+Register4* register4_create();
+Register8* register8_create();
 
 void register1_destroy(Register1* r);
+void register2_destroy(Register2* r);
+void register4_destroy(Register4* r);
+void register8_destroy(Register8* r);
 
 void register1_write(Register1* r, bool i, bool ck);
+void register2_write(Register2* r, Bit2 i, bool ck);
+void register4_write(Register4* r, Bit4 i, bool ck);
+void register8_write(Register8* r, Bit8 i, bool ck);
 
 bool register1_read(Register1* r);
+Bit2 register2_read(Register2* r);
+Bit4 register4_read(Register4* r);
+Bit8 register8_read(Register8* r);
 
 #endif
